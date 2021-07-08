857791651766aac99457694c74eeb1866177a7a3
mhacmoun <84976627+mhacmoun@users.noreply.github.com>
2021-07-08 22:32:38 +0100

    un changement juste pour essayer

--- generator.js
+++ generator.js
@@ -6,3 +6,4 @@ const thirdtarray = ["week", "day", "holiday"];
 const generated_inspirational_message = firstarray[Math.floor(Math.random() * "3")] + " " + secondtarray[Math.floor(Math.random() * "3")] + " " + thirdtarray[Math.floor(Math.random() * "3")];
 
 console.log(generated_inspirational_message);
+// un changement
