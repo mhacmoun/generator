1927f9f017c5d42a4fcd6ea16c4f523ea3308bf5
mhacmoun <84976627+mhacmoun@users.noreply.github.com>
2021-07-11 00:51:15 +0100

    change

--- generator.js
+++ generator.js
@@ -1,9 +1,8 @@
-const firstarray = ["good morning", "good afternoon", "good evening"];
-const secondtarray = ["have a good", "have a wonderfull", "have a nice"];
-const thirdtarray = ["week", "day", "holiday"];
+const firstarray = ["good morning", "good afternoon", "good evening", "good night"];
+const secondarray = ["have a good", "have a wonderfull", "have a nice", "have a nice rest"];
+const thirdarray = ["week", "day", "holiday", "weekend"];
 
 
-const generated_inspirational_message = firstarray[Math.floor(Math.random() * "3")] + " " + secondtarray[Math.floor(Math.random() * "3")] + " " + thirdtarray[Math.floor(Math.random() * "3")];
+const generated_inspirational_message = firstarray[Math.floor(Math.random() * "3")] + " " + secondarray[Math.floor(Math.random() * "3")] + " " + thirdarray[Math.floor(Math.random() * "3")];
 
 console.log(generated_inspirational_message);
-// un changement
